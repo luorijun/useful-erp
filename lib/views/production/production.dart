39f6db6d377766c9846d69ace809ca0fced32867
luorijun <luorijun@outlook.com>
2022-03-17 11:18:49 +0800

    新增规格和单位字段

--- lib/views/production/production.dart
+++ lib/views/production/production.dart
@@ -1,16 +1,20 @@
 class Production {
   late int? id;
   late String? name;
+  late String? spec;
   late double? price;
   late double? cost;
+  late String? unit;
   late DateTime? createAt;
   late int? createBy;
 
   Production({
     this.id,
     this.name,
+    this.spec,
     this.price,
     this.cost,
+    this.unit,
     this.createAt,
     this.createBy,
   });
--- lib/views/production/production_fom.dart
+++ lib/views/production/production_fom.dart
@@ -4,25 +4,21 @@ import 'package:useful_erp/views/production/production.dart';
 
 class ProductionFormState extends ChangeNotifier {
   ProductionFormState([Production? production]) {
-    _production = production;
+    _production = production ?? Production();
   }
 
-  late final Production? _production;
+  late final Production _production;
 
   Production get production {
-    if (_production != null) {
-      return Production(
-        createAt: DateTime.now(),
-      );
-    }
-
     return Production(
-      id: _production!.id,
-      name: _production!.name,
-      cost: _production!.cost,
-      price: _production!.price,
-      createAt: _production!.createAt ?? DateTime.now(),
-      createBy: _production!.createBy,
+      id: _production.id,
+      name: _production.name,
+      spec: _production.spec,
+      cost: _production.cost,
+      price: _production.price,
+      unit: _production.unit,
+      createAt: _production.createAt ?? DateTime.now(),
+      createBy: _production.createBy,
     );
   }
 }
@@ -32,16 +28,20 @@ class ProductionForm extends StatelessWidget {
     Key? key,
     required this.state,
   }) : super(key: key) {
-    _name = TextEditingController(text: state.production.name);
-    _cost = TextEditingController(text: state.production.cost?.toString() ?? '0');
-    _price = TextEditingController(text: state.production.price?.toString() ?? '0');
+    _name = TextEditingController(text: state._production.name);
+    _spec = TextEditingController(text: state._production.spec);
+    _cost = TextEditingController(text: state._production.cost?.toString() ?? '0');
+    _price = TextEditingController(text: state._production.price?.toString() ?? '0');
+    _unit = TextEditingController(text: state._production.unit);
   }
 
   final ProductionFormState state;
 
   late final TextEditingController _name;
+  late final TextEditingController _spec;
   late final TextEditingController _cost;
   late final TextEditingController _price;
+  late final TextEditingController _unit;
 
   @override
   Widget build(BuildContext context) {
@@ -50,18 +50,19 @@ class ProductionForm extends StatelessWidget {
       children: [
         TextFormField(
           controller: _name,
-          decoration: const InputDecoration(
-            labelText: "名称",
-          ),
+          decoration: const InputDecoration(labelText: "名称"),
           onChanged: (_) => state.production.name = _name.text,
+        ),
+        TextFormField(
+          controller: _spec,
+          decoration: const InputDecoration(labelText: "规格"),
+          onChanged: (_) => state.production.spec = _name.text,
         ),
         TextFormField(
           controller: _cost,
           inputFormatters: [numberFormatter],
           keyboardType: TextInputType.number,
-          decoration: const InputDecoration(
-            labelText: "成本",
-          ),
+          decoration: const InputDecoration(labelText: "成本"),
           onChanged: (_) {
             if (_cost.text.isEmpty) return;
             state.production.cost = double.parse(_cost.text);
@@ -71,13 +72,16 @@ class ProductionForm extends StatelessWidget {
           controller: _price,
           inputFormatters: [numberFormatter],
           keyboardType: TextInputType.number,
-          decoration: const InputDecoration(
-            labelText: "售价",
-          ),
+          decoration: const InputDecoration(labelText: "售价"),
           onChanged: (_) {
             if (_price.text.isEmpty) return;
             state.production.price = double.parse(_price.text);
           },
+        ),
+        TextFormField(
+          controller: _unit,
+          decoration: const InputDecoration(labelText: "单位"),
+          onChanged: (_) => state.production.name = _name.text,
         ),
       ],
     );
